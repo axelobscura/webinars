30525f739a58a8c5ffcacf7b814deb31ed292218
axelobscura <axosar@gmail.com>
2020-10-08 19:49:55 -0500

    El titulo qs

--- pages/quienes-somos.js
+++ pages/quienes-somos.js
@@ -19,7 +19,7 @@ const columnaQS = {
     alignItems: 'center',
     flex: '30em 1',
     background: 'rgb(0,38,50)',
-    background: 'linear-gradient(306deg, rgba(0,38,50,1) 0%, rgba(2,99,128,1) 100%)'
+    background: 'linear-gradient(306deg, rgba(0,38,50,1) 0%, rgba(2,99,128,1) 100%)',
 }
 
 const columnaQSTitulo = {
@@ -48,6 +48,12 @@ const columnaQSp = {
     fontWeight: '100'
 }
 
+const columnaQSh3 = {
+    padding: '10px 30px',
+    color: '#fff',
+    textAlign: 'center'
+}
+
 export default function Quienessomos() {
     return (
         <Layout fluid>
@@ -57,7 +63,12 @@ export default function Quienessomos() {
                         <h1 style={columnaQSh1}>QUIÉNES SOMOS</h1>
                     </Col>
                     <Col style={columnaQS}>
+                        <div>
+                        <h3 style={columnaQSh3}>Instituto Mexicano del Cemento y del Concreto A.C.</h3>
+                        <hr/>
                         <p style={columnaQSp}><b>El IMCYC, A.C.</b> es una asociación no lucrativa dedicada a la <b>investigación, enseñanza y difusión de las técnicas de aplicación del cemento y del concreto</b>. Su misión es promover la utilización óptima del cemento y del concreto para satisfacer las necesidades del mercado con calidad, productividad y oportunidad, contribuyendo a mejorar el desempeño profesional, el desarrollo y beneficio económico de la industria, así como de la sociedad.<br/><br/><b>El IMCYC surgió en 1923</b> como un «Comité para Propagar el Uso del Cemento Portland». Ya en 1959 se constituyó como Instituto Mexicano del Cemento y del Concreto, A.C., con el objetivo de <b>investigar y promover métodos constructivos y soluciones técnicas que permitan un uso más económico y racional del concreto en la construcción</b>, para ofrecer el concreto al diseñador, al constructor y al propietario como la alternativa más ventajosa frente al empleo de otros materiales, además de crear más y mejores satisfactores para los asociados y sus clientes.<br/><br/>De este modo, se aportan nuevos conocimientos sobre el cemento y el concreto, y se enseña a trabajarlo con calidad, resolviendo los problemas de aplicación en las obras y orientando a los profesionales en sus múltiples oportunidades de utilización. Así mismo, se fortalecen nuestras estructuras, desarrollando al personal, ampliando nuestra presencia nacional e internacional y haciendo más efectivas y eficientes las operaciones. <b>El IMCYC es imagen, voz y representatividad del Sector Cemento y Concreto ante la sociedad, las autoridades nacionales y entidades internacionales, no estando comprometido con ninguna de las empresas del ramo</b>.</p>
+                        </div>
+                        
                     </Col>
                 </Row>
             </Container>
